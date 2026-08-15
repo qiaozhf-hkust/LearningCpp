88ea325fcbd8d6d07692ece144167f4f9f4cc3f6
Zhefeng Qiao <zqiaoaa@connect.ust.hk>
2026-08-15 22:03:32 +0800

    第3步：支持functor自定义资源释放器

--- W1/D5/UniqueResource.h
+++ W1/D5/UniqueResource.h
@@ -1,9 +1,11 @@
-template<typename Resource>
+#include <cstdio>
+
+template<typename Resource, typename Deleter>
 class UniqueResource {
 public:
-    explicit UniqueResource(Resource resource) noexcept : resource_(resource) {}
+    explicit UniqueResource(Resource resource, Deleter deleter) noexcept : resource_(resource), deleter_(deleter) {}
     ~UniqueResource() { 
-        if (resource_) release(resource_); 
+        if (resource_) deleter_(resource_); 
     }
     
     UniqueResource(const Resource& resource) = delete;
@@ -12,5 +14,19 @@ public:
     Resource get() const noexcept { return resource_; }
 private:
     Resource resource_;
-    void release(); // ?
+    Deleter deleter_;
 };
+
+struct file_closer {
+    void operator()(FILE* f) { 
+        if (f){
+            std::fclose(f);
+        }
+    }
+};
+
+using FileHandler = UniqueResource<FILE*, file_closer>;
+
+FileHandler open(const char* path, const char* mode){
+    return FileHandler(std::fopen(path, mode), file_closer{});
+}
